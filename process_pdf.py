091261ccb90cb2350277fdc6b2701022abe7fcff
Danny Perkins <48068870+dgp3sy@users.noreply.github.com>
2019-06-12 20:20:29 -0500

    Update process_pdf.py

--- process_pdf.py
+++ process_pdf.py
@@ -46,7 +46,7 @@ def new_file_name_generator(filepath):
     # Defines Regular Expressions for finding following information
     month_findr = re.compile(r"(January | February | March | April | May | June | July | August | September | October | November | December)")
     policyNo_findr = re.compile(r"Policy No\. ([0-9/-]*)")
-    name_findr = re.compile(r"Policyowner(.*?)(\n*?)DEAN E HARDER ([A-Z _]*)")
+    name_findr = re.compile(r"Policyowner(.*?)(\n*?)ADVISOR_NAME_HERE ([A-Z _]*)")
     name_findr2 = re.compile(r"Policyholder:\n[A-Za-z ]*[0-9]*([A-Za-z ]*)")
 
     # Initiates list to Store save names of each individual page
