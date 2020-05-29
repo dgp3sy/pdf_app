b0a1505ccc127f4fad88e5d97142ddb8b6fe65d7
Danny Perkins <dgp3sy>
2020-05-29 13:34:16 -0400

    updated UI and added save file

--- user_interface.py
+++ user_interface.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog as fd
 import process_pdf
+import ido_reader
 import time
 
 
@@ -48,21 +49,29 @@ def call_proccess_pdf():
     :return: None, although displays print to console if there is a successful job completed
     """
     global files
-    global var1
+    global doc_type
     # output time for diagnostic purposes only - can be implemented by the user if desired
     # start_time = time.time()
     no_errors=True
     for each_file in files:
         try:
-            if var1.get():
-                # print("new proccess being filed")
+            # Document type determined by radio buttons:
+            #   1 = New Annual Report
+            #   2 = Old Annual Report
+            #   3 = IDO Letter
+            doc_type_int = doc_type.get()
+            if doc_type_int == 1:
                 process_pdf.init_new(each_file)
-            else:
-                # print("old process being filed")
+            elif doc_type_int == 2:
                 process_pdf.init_old(each_file)
+            elif doc_type_int == 3:
+                ido_reader.init_ido_reader(each_file)
+            else:
+                raise
         except:
             no_errors=False
             print("ERROR: Unable to process file: ", each_file)
+            raise
     if(no_errors):
         print("All files successfully processed!")
     # print("--- %s seconds ---" % (time.time() - start_time))
@@ -88,16 +97,21 @@ file_browser = Browse(root, initialdir=r"C:\Users", filetypes=filetypes)
 
 # defines the submit button to start the process, calls the function on every file passed through the file browser
 submit_button = tk.Button(root, text="Submit", command=call_proccess_pdf)
-
-# Determines the type of document
-var1 = tk.IntVar()
-doc_type = tk.Checkbutton(root, text="New", variable=var1, onvalue = 1, offvalue = 0, height=5, width = 10)
-
+# Determines the type of document with radio buttons
+doc_type = tk.IntVar()
+is_new = tk.Radiobutton(root, text="New Annual Report",
+                          padx = 20, variable = doc_type,value = 1)
+is_old = tk.Radiobutton(root, text="Old Annual Report",
+                        padx = 20, variable = doc_type, value = 2)
+is_IDO = tk.Radiobutton(root, text="IDO Letter",
+                        padx = 20, variable = doc_type, value = 3)
 # TODO: Selection of the specific file path rather than the root directory of this code
 
 # Displays UI to the screen
 select_file_label.pack()
 file_browser.pack(fill='x', expand=True)
-doc_type.pack()
+is_new.pack()
+is_old.pack()
+is_IDO.pack()
 submit_button.pack(fill='x')
 root.mainloop()
